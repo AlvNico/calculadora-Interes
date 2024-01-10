c6f6df036abb09be0ab9902bda0e31569d14c45c
AlvNico <alvareznicolasok@gmail.com>
2024-01-10 17:02:59 -0300

    first

---
+++ src/components/Balance.js
@@ -0,0 +1,12 @@
+import styled from 'styled-components'
+
+const Balance=styled.span`
+
+    color:black;
+    font-size: 36px;
+    margin-top:12px;
+    display:inline-block;
+  
+`
+
+export default Balance
